c54b6257f11795f865fb799ea61ccf01219554e9
agr <agarcesr@eafit.edu.co>
2025-09-03 23:05:02 -0500

    feat: add Specialty enum to represent referee roles (COURT, TABLE)

---
+++ CABAPRO/src/main/java/com/cabapro/development/model/Specialty.java
@@ -0,0 +1,14 @@
+/**
+ * Enum representing the specialty of a referee.
+ * Indicates whether the referee works on the court or at the table.
+ *
+ * Author: Alejandro Garcés Ramírez
+ * Date: 2025-09-04
+ * Role: Domain enum - Specialty
+ */
+package com.cabapro.development.model;
+
+public enum Specialty {
+    COURT,  // Referee on the court
+    TABLE   // Referee at the scorer's table
+}
